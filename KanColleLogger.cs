306ef7d8837f1bac17a753416f1d3152edd843c7
vernon91 <vernon0591@gmail.com>
2015-05-18 00:41:18 +0800

    Initial release
    Combined fleet battlelog

---
+++ KanColleLogger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+using Grabacr07.KanColleViewer.Composition;
+using Grabacr07.KanColleWrapper;
+
+namespace Logger
+{
+	[Export(typeof(IToolPlugin))]
+	[ExportMetadata("Title", "KanColleLogger")]
+	[ExportMetadata("Description", "File logging back-end")]
+	[ExportMetadata("Version", "1.0")]
+	[ExportMetadata("Author", "@Xiatian")]
+	public class KanColleLogger : IToolPlugin
+	{
+		private readonly LoggerViewModel viewmodel = new LoggerViewModel
+		{
+			Loggers = new ObservableCollection<LoggerBase>
+			{
+				new ItemLog(KanColleClient.Current.Proxy),
+				new ConstructionLog(KanColleClient.Current.Proxy),
+				new BattleLog(KanColleClient.Current.Proxy),
+				new MaterialsLog(KanColleClient.Current.Proxy),
+                new ExpedLog(KanColleClient.Current.Proxy),
+			}
+		};
+
+		public string ToolName
+		{
+			get { return "Logger"; }
+		}
+
+		public object GetSettingsView()
+		{
+			return null;
+		}
+
+		public object GetToolView()
+		{
+			return new LoggerView { DataContext = this.viewmodel, };
+		}
+	}
+}
